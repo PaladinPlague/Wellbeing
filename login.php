6a72f230c3652e1aa9e77bba3461b0ea929c597d
iyb19168 <callum.turnbull.2019@uni.strath.ac.uk>
2022-03-14 19:01:16 +0000

    Changing form submissions to use AJAX, with a redirect

--- login.php
+++ login.php
@@ -1,10 +1,10 @@
 <?php
 function safePOST($conn, $name){
-    if (isset($_POST[$name])) {
-        return $conn->real_escape_string(strip_tags($_POST[$name]));
-    } else {
-        return "";
-    }
+  if (isset($_POST[$name])) {
+    return $conn->real_escape_string(strip_tags($_POST[$name]));
+  } else {
+    return "";
+  }
 }
 
 require_once "database_query.php";
@@ -16,13 +16,13 @@ $password = safePOST($conn, "password");
 
 $passwordHash = $password ? password_hash($password, PASSWORD_DEFAULT) : "";
 
-$sql = "SELECT EXISTS(SELECT `user_id` FROM `users` WHERE `username` = '$username')";
+$sql = "SELECT `user_id` FROM `users` WHERE `username` = '$username'";
 $result = execute_query($conn, $sql);
 
 if ($result->num_rows > 0) {
-        echo "exists!";
+  echo 1;
 } else {
-    die("User not in database.");
+  echo 0;
 }
 
 $conn->close();
