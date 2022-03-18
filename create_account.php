5d8fa88b30c9969aee682379e331ff6636852151
Euan Marsh <euan.marsh.2019@uni.strath.ac.uk>
2022-03-18 13:48:51 +0000

    improved login

--- create_account.php
+++ create_account.php
@@ -19,6 +19,18 @@ $passwordHash = $password ? password_hash($password, PASSWORD_DEFAULT) : "";
 $sql = "INSERT INTO users(username, password_hash) VALUES('$username', '$passwordHash')";
 
 
+$user_check_query = "SELECT * FROM users WHERE username='$username'";
+$result2 = execute_query($conn, $user_check_query);
+$user = mysqli_fetch_assoc($result2);
+
+if ($user) { // if user exists
+    if ($user['username'] === $username) {
+        //will work on making this part of the form.
+        echo("Username already exists.");
+        exit();
+    }
+}
+
 if(strlen($username) <=255 && strlen($password) <= 255) {
   $result = execute_query($conn, $sql);
 
--- login.php
+++ login.php
@@ -16,13 +16,41 @@ $password = safePOST($conn, "password");
 
 $passwordHash = $password ? password_hash($password, PASSWORD_DEFAULT) : "";
 
-$sql = "SELECT EXISTS(SELECT `user_id` FROM `users` WHERE `username` = '$username')";
+$sql = "SELECT `user_id` FROM `users` WHERE `username` = '$username'";
 $result = execute_query($conn, $sql);
+$ID = mysqli_fetch_assoc($result);
+$userID = $ID['user_id'];
 
 if ($result->num_rows > 0) {
-        echo "exists!";
+        echo "user exists!";
+        $hash = "SELECT `password_hash` FROM `users` WHERE `username` = '$username'";
+    $result2 = execute_query($conn, $hash);
+    $array = mysqli_fetch_assoc($result2);
+    $resultString = $array['password_hash'];
+    if (password_verify($password, $resultString)) {
+        echo 'Password is valid!';
+        //add a logout button on main page that clears local storage so until user logs out they will always be signed in.
+        ?>
+        <script>
+              local = window.localStorage;
+              id = <?php echo $userID; ?>;
+              local.setItem("ID", id);
+        </script>
+        <?php
+        header('location: get_post.php');
+    } else {
+        $passwordError = "Incorrect Password";
+        echo "<script type='text/javascript'>alert('$passwordError');</script>";
+        sleep(5);
+        //ask others best way to redirect to login form with an incorrect password message
+        //header('location: login.html');
+        //die();
+    }
 } else {
-    die("User not in database.");
+    echo ("User not in database.");
+    sleep(3);
+    header('location: login.html');
+    die();
 }
 
 $conn->close();
